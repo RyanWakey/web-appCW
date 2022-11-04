964808e5625063eff2fe084b1838cfb4f5c9edfb
Ryan Wake <wake52@outlook.com>
2022-11-04 00:31:55 +0000

    added seeder to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+       
+        $this->call(MemberTableSeeder::class);
+        $this->call(ProfileTableSeeder::class); 
+        $this->call(PostTableSeeder::class); 
+   
+       
+       
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
