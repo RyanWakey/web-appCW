5ca1cffc61b9eb79eea604ac762b60da1309d583
Ryan Wake <wake52@outlook.com>
2022-12-02 22:39:26 +0000

    Added show action for different posts based on id

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.basic')
+
+@section('title','Post')
+
+@section('content')
+  {{$post}}
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
