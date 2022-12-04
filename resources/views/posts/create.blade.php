c479b056fa4490ca8b02b5049dfb294728eeea6b
Ryan Wake <wake52@outlook.com>
2022-12-04 03:12:18 +0000

    keeps old values on failed inputs

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,8 +7,10 @@
 <p> Enter the details below to create a Post</p>
     <form method="POST" action="{{route('posts.store')}}">
         @csrf
-        <p>Title: <input type="text" name="title"></p>
-        <p>Description: <input type="text" name="description"></p>
+        <p>Title: <input type="text" name="title"
+            value="{{ old('title')}}"></p>
+        <p>Description: <input type="text" name="description"
+            value="{{ old('description')}}"></p>
         <input type="submit" value="Submit">
         <a href="{{route('posts.index') }}">Cancel</a>
 
