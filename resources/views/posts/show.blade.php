338191013c637ac50ff7b4d597180cc49b782078
Ryan Wake <wake52@outlook.com>
2022-12-04 20:39:47 +0000

    can now delete a post from post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,4 +8,11 @@
         Title: {{$post->title}}</li>
         <li>Description {{$post->description}}</li>    
     </ul>
+
+    <form method="POST"
+        action="{{route('posts.destroy',['id' => $post->id]) }}">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete Post</button>
+    </form>
 @endsection
