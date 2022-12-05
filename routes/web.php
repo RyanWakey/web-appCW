9429416998f03853740b851db4b41ac8c3ab9c42
Ryan Wake <wake52@outlook.com>
2022-12-05 23:06:12 +0000

    fixed route logic

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,10 @@ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
-Route::get('/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/posts/{post}/create', [CommentController::class, 'create'])->name('comments.create');
+
+Route::post('/post/{post}/comment', [CommentController::class, 'store'])->name('comments.store');
+
 Route::delete('/comments/{comment}', [PostController::class, 'destroy'])->name('comments.destroy');
 
 Route::get('/dashboard', function () {
