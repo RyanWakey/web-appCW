294bf67bd37b58abd67fbe322debe0b12ed18341
Ryan Wake <wake52@outlook.com>
2022-12-10 18:18:49 +0000

    Added login ability to all views + a redirect route

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -3,10 +3,23 @@
 <head> 
     <title>Blog - @yield('title')</title>
 </head>
-
-<body>
-    <h1>Blog - @yield('title')</h1>
+<h1>Blog - @yield('title')</h1>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
     
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            
     @if ($errors->any())
         <div>
             Errors:
@@ -25,5 +38,6 @@
     <div>
         @yield('content')
     </div>
+</body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/' , 'posts');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
