413c9eda1d32932611fb46f2fbdfe02175183800
Ryan Wake <wake52@outlook.com>
2022-12-10 23:28:57 +0000

    Created edit form

---
+++ resources/views/comments/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.basic')
+
+@section('title','New Comment')
+    
+
+@section('content')
+<p> Comment on this Post:</p>
+    <form method="POST" action="{{route('comments.update',['post' => $post])}}">
+        <input type="hidden" name="post_id" id="post_id" value="{{ $post->id }}" />
+        @csrf
+        <p>Description: <input type="text" name="description"
+            value="{{ old('description')}}"></p>
+        <input type="submit" value="Submit">
+        <a href="{{ route('posts.show',['post' => $post])}}">Cancel</a>
+    </form>
+@endsection
