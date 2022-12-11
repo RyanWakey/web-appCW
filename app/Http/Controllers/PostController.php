fdf0141be501b0909e528d65c0238a66e51a7c18
Ryan Wake <wake52@outlook.com>
2022-12-11 02:25:31 +0000

    Added post edit/update methods to controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,9 +75,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('posts.edit', ['post' => $post]);
     }
 
     /**
@@ -87,9 +87,18 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|max:150',
+            'description' => 'required|max:500',
+        ]);
+
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        
+        return redirect()->route('posts.show', ['post' => $post])->with('message', 'Post was Updated');
     }
 
     /**
