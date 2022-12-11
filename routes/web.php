22992789419acbe174574bde906efb67904ab521
Ryan Wake <wake52@outlook.com>
2022-12-11 02:24:51 +0000

    Added routes for edit/update for post

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware(['auth']);
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware(['auth']);
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit')->middleware(['auth']);
+Route::patch('/posts/{post}' , [PostController::class, 'update'])->name('posts.update')->middleware(['auth']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy')->middleware(['auth']);
 
 Route::get('/posts/{post}/create', [CommentController::class, 'create'])->name('comments.create')->middleware(['auth']);
