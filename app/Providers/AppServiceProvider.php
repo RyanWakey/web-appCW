5057b4bd308b0e4c22775794b58f70247b61b301
Ryan Wake <wake52@outlook.com>
2022-12-12 23:23:51 +0000

    Added macro which allows paginating collections

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,11 +20,26 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
+     * Copied this method from github gist, its a solution which allows me to paginate 
+     * collections as normal paginatition dosent work with collections
+     * https://gist.github.com/simonhamp/549e8821946e2c40a617c85d2cf5af5e 
      * @return void
      */
     public function boot()
     {
-        //
+        Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
     }
 }
