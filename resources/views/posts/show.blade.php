f0c8335e64d360820d90d3898f41a8f93b79916d
Ryan Wake <wake52@outlook.com>
2022-12-18 21:43:07 +0000

    show post css

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,54 +13,72 @@
     <div class="flex-auto px-4 py-2 text-base bg-orange-600 text-green-500">
         <ul>
             Post created by User: 
-            <span class="text-purple-900 text-semibold italic underline">
+            <span class="text-purple-900 font-extrabold italic underline">
                 <a href="{{route('users.show',['user' => $post->user, 'post' => $post])}}">       
                 {{$post->user->name}}</a><br>
             </span>
             Title:  {{$post->title}}<br>
             Description: {{$post->description}}<br><br>
         </ul>
-        
-        <div class="flex space-x-4 mb-6 text-sm font-medium">
-            <div class="flex-auto flex space-x-20">
-                @if(!Auth::user())
-                @else 
-                    @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
-                        <button class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-white" type="button">
-                            <a href="{{route('posts.edit', ['post' => $post])}}">Edit Post  </a>
-                        </button>    
-                    @endif
-                @endif
+    </div>
 
-                @if(!Auth::user())
+
+    <div class="flex space-x-4 mb-6 text-sm font-medium">
+        <div class="flex-auto flex space-x-20 bg-orange-600">
+            @if(!Auth::user())
                 @else 
-                    @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
-                        <form method="POST" action= "{{route('posts.destroy',['post' => $post->id]) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button class="h-10 px-6 font-black rounded-md border border-black text-slate-900 bg-white" 
-                        type="submit">Delete Post</button>
-                        </form>
-                    @endif
+                @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
+                    <button class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-white" type="button">
+                        <a href="{{route('posts.edit', ['post' => $post])}}">Edit Post  </a>
+                    </button>   
                 @endif
-            </div>
+            @endif
+
+            @if(!Auth::user())
+            @else 
+                @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
+                    <form method="POST" action= "{{route('posts.destroy',['post' => $post->id]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="h-10 px-6 font-black rounded-md border border-black text-slate-900 bg-white" 
+                    type="submit">Delete Post</button>
+                    </form>
+                @endif
+            @endif
         </div>
     </div>
+    
+
+
 
     <div class="font-mono text-2xl font-semibold py-4">
         Comments: <br>
     </div>
+    
+        <ul>
+            @foreach($post->comments as $comment)
+                <div class="flex-auto px-4 text-base bg-orange-600 text-green-500">
+                    User: 
+                    <span class="text-purple-900 font-extrabold italic underline">
+                        <a href="{{route('users.show',['user' => $post->user, 'post' => $post])}}"> 
+                        {{$comment->user->name}}<br>
+                    </span>
+                    <span class="text-purple-900 font-extrabold underline">
+                    <a href="{{route('comments.show', ['post' => $post, 'comment' => $comment])}}"> 
+                        Comment: </a> {{$comment->description}}<br>
+                </div> 
+                <br>
+            @endforeach
+        </ul>
+    
+        
+    <button class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-white" type="button">
+        <a href="{{ route('comments.create', ['post' => $post])}}">Create a Comment</a>
+    </button>
+    
+    <button class="h-10 px-6 font-semibold rounded-md border border-black text-slate-900 bg-white" type="button">
+        <a href="{{ route('posts.index')}}">Return</a>
+    </button>
 
-    <div class="flex-auto px-4 py-2 text-base bg-orange-600 text-green-500">
-        @foreach($post->comments as $comment)
-            <ul>
-                User: {{$comment->user->name}} <br>
-            <a href="{{route('comments.show', ['post' => $post, 'comment' => $comment])}}"> Comment: </a> {{$comment->description}} <br>
-            </ul>  
-        @endforeach
-
-        <a href="{{ route('comments.create', ['post' => $post])}}">Create a Comment</a><br><br><br>
-    </div>
-    <a href="{{ route('posts.index')}}">Return</a><br><br><br>
 </div>
 @endsection
