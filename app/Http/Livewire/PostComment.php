e2540f0ca49b37e669537279b490febfa7336e45
Ryan Wake <wake52@outlook.com>
2022-12-19 06:55:55 +0000

    Attempted Livewire for adding comments

--- app/Http/Livewire/PostComment.php
+++ app/Http/Livewire/PostComment.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class PostComment extends Component
 {
+    public function storeComment(Request $request,Post $post)
+    {   
+        $validatedData = $request->validate([
+            'description' => 'required|max:500',
+        ]);
+        
+        $comment = new Comment;
+        $comment->description = $request->description;
+        $comment->user_id = auth()->user()->id; 
+        $comment->post_id = $request->input('post_id');
+        $comment->save();
+ 
+    }
+
     public function render()
     {
         return view('livewire.post-comment');
--- resources/views/livewire/post-comment.blade.php
+++ resources/views/livewire/post-comment.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div style="text-align: Center">
+    <h1> Comment on this Post: </h1>
+    <form wire:submit.prevent="storeComment">
+        <input type="text" name="description" value="{{ old('description')}}" 
+            style="height:100px;font-size:12pt;width:500px;"></p><br>
+        <button class="h-10 px-6 mb-4 font-semibold rounded-md bg-black text-white"
+            type="submit" value="Submit">Add Comment
+        </button>
+    </form>
 </div>
