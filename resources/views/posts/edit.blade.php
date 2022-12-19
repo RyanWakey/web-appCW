323d022e114494c59ec29cec0d065f1aa2613fdd
Ryan Wake <wake52@outlook.com>
2022-12-19 00:05:25 +0000

    css edit post button

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,20 +4,36 @@
     
 @section('content')
 
-@if(!Auth::user())
-@else 
-    @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
-        <p> Edit Comment:</p>
-        <form method="POST" action="{{route('posts.update',['post' => $post])}}">
-            @csrf
-            @method('patch')
-            <p>title: <input type="text" name="title" value="{{ old('title')}}"></p>
-            <p>description: <input type="text" name="description" value="{{ old('description')}}"></p>
-            <button type="submit">Edit Comment</button> 
-            <a href="{{ route('posts.show',['post' => $post])}}">Cancel</a>
-        </form> 
-    @endif
-@endif
+<div class="font-mono text-2xl">
+    <p> Edit Post:</p>
+</div>  
 
+<div class="px-6 py-2 bg-red-600">
+        @if(!Auth::user())
+        @else 
+        @if(auth()->user()->id == $post->user->id || auth()->user()->admin == true)
+            <form method="POST" action="{{route('posts.update',['post' => $post])}}">
+                @csrf
+                @method('patch')
+                <div class="flex-auto space-x-4 mb-6 text-lg font-medium">
+                    <p>Title:    <input type="text" name="title" value="{{ old('title')}}"
+                        style="height:200px;font-size:12pt;width:1100px"></p>
+                </div>
+                <div class="space-x-4 py-4 text-lg font-medium">    
+                    <p>Description: <input type="text" name="description" value="{{ old('description')}}"" 
+                        style="height:420px;font-size:12pt;width:1100px"></p><br>
+                <div>
+               
+                <button class="h-10 px-6 mb-4 font-semibold rounded-md border border-black text-slate-900 bg-white" 
+                    type="submit">Edit Post
+                </button>
+
+                <button class="h-10 px-6 mb-4 font-semibold rounded-md border border-black text-slate-900 bg-white"> 
+                    <a href="{{ route('posts.show',['post' => $post])}}">Cancel</a>
+                </button>
+            </form> 
+        @endif
+    @endif
+</div>
 
 @endsection
