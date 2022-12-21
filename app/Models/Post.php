92825a7bac83ae5ff6dfe99186b40621f6368896
Ryan Wake <wake52@outlook.com>
2022-12-21 06:40:19 +0000

    Created Tag Model + added relationship - post+tag (and migration for tag)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,4 +18,8 @@ class Post extends Model
     public function comments() {
         return $this->hasMany(Comment::class); 
     }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::Class);
+    }
 }
